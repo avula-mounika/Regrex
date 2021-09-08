a000196858b64c34043b460cfb1929dd6ac88b48
avula-mounika <goudmounika13@gmail.com>
2021-09-08 15:32:53 +0530

    Uc-1

---
+++ PinCode.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCode {
+
+	public static void main(String[] args) {
+		
+		String zipCode = "400088";
+		String regex = "^[0-9]{6}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(zipCode);
+		
+		System.out.println("Is Valid :"+matcher.matches());
+		
+
+	}
+
+}
